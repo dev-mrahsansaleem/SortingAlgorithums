4b582f163359ccc6a5f046fccb2e3390b85f9cd1
Ahsan Saleem <ahsansaleemreal@gmail.com>
2019-11-09 16:38:38 +0500

    shell sort implimented

--- sorting algorithms/sorting algorithms/Source.cpp
+++ sorting algorithms/sorting algorithms/Source.cpp
@@ -146,6 +146,33 @@ void radixsort(int arr[], int n)
 	for (int exp = 1; m / exp > 0; exp *= 10)
 		countSort(arr, n, exp);
 }
+int shellSort(int arr[], int n)
+{
+	// Start with a big gap, then reduce the gap 
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+		// Do a gapped insertion sort for this gap size. 
+		// The first gap elements a[0..gap-1] are already in gapped order 
+		// keep adding one more element until the entire array is 
+		// gap sorted  
+		for (int i = gap; i < n; i += 1)
+		{
+			// add a[i] to the elements that have been gap sorted 
+			// save a[i] in temp and make a hole at position i 
+			int temp = arr[i];
+
+			// shift earlier gap-sorted elements up until the correct  
+			// location for a[i] is found 
+			int j;
+			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+				arr[j] = arr[j - gap];
+
+			//  put temp (the original a[i]) in its correct location 
+			arr[j] = temp;
+		}
+	}
+	return 0;
+}
 int main()
 {
 	const int size = 10;
@@ -158,30 +185,35 @@ int main()
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto bubbleduration = duration_cast<microseconds>(t2 - t1).count();
 	printArray(arr, size);
-	
+
+	cout << "selection sort" << endl;
 	randomGenrator(arr, size);
 	t1 = std::chrono::high_resolution_clock::now();
 	selectionSort(arr, size);
 	t2 = std::chrono::high_resolution_clock::now();
 	auto selectionduration = duration_cast<microseconds>(t2 - t1).count();
 	printArray(arr, size);
-	
+
+	cout << "heap sort" << endl;
 	randomGenrator(arr, size);
 	t1 = std::chrono::high_resolution_clock::now();
 	heapSort(arr, size);
 	t2 = std::chrono::high_resolution_clock::now();
 	auto heapduration = duration_cast<microseconds>(t2 - t1).count();
 	printArray(arr, size);
-	
+
+	cout << "radix sort" << endl;
 	randomGenrator(arr, size);
 	t1 = std::chrono::high_resolution_clock::now();
 	radixsort(arr, size);
 	t2 = std::chrono::high_resolution_clock::now();
 	auto radixduration = duration_cast<microseconds>(t2 - t1).count();
 	printArray(arr, size);
-	
+
+	cout << "shell sort" << endl;
 	randomGenrator(arr, size);
 	t1 = std::chrono::high_resolution_clock::now();
+	shellSort(arr, size);
 	t2 = std::chrono::high_resolution_clock::now();
 	auto shellduration = duration_cast<microseconds>(t2 - t1).count();
 	printArray(arr, size);
